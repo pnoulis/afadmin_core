66c39a1141263cdc800ff0550da5fd032f56c5cf
pnoul <pavlos.noulis@gmail.com>
2023-03-14 13:04:28 +0200

    task runner refactoring and new tests

--- src/task_runners/TaskRunner.js
+++ src/task_runners/TaskRunner.js
@@ -1,3 +1,10 @@
+class TaskRunnerError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
 const States = {};
 States.Idle = function Idle(taskRunner) {
   this.name = "idle";
@@ -5,16 +12,8 @@ States.Idle = function Idle(taskRunner) {
   this.getState = () => this;
 };
 
-States.Idle.prototype.run = function run(task, timeout) {
-  return new Promise((resolve, reject) => {
-    const job = {
-      timeout,
-      done: false,
-      exec: () => task().then(resolve, reject),
-    };
-    this.taskRunner.queue(job);
-    this.taskRunner.setState(this.taskRunner.states.pending.getState());
-  });
+States.Idle.prototype.run = function run(job) {
+  return job(() => this.taskRunner.setState("pending"));
 };
 
 States.Pending = function Pending(taskRunner) {
@@ -24,33 +23,27 @@ States.Pending = function Pending(taskRunner) {
 };
 
 States.Pending.prototype.init = function init() {
+  console.log("pending state init");
   this.poll();
 };
 
 States.Pending.prototype.poll = function poll() {
   this.intervalID = setInterval(() => {
-    if (this.state === "online") {
+    console.log("polling");
+    if (this.taskRunner.inState("online")) {
       return clearInterval(this.intervalID);
     }
 
     this.taskRunner.flush();
     if (this.taskRunner.jobQueue.length === 0) {
       clearInterval(this.intervalID);
-      this.taskRunner.setState(this.taskRunner.states.idle.getState());
+      this.taskRunner.setState("idle");
     }
   }, 1000);
 };
 
-States.Pending.prototype.run = function run(task, timeout) {
-  return new Promise((reslove, reject) => {
-    const job = {
-      timeout,
-      done: false,
-      exec: () => task().then(resolve, reject),
-    };
-
-    this.taskRunner.queue(job);
-  });
+States.Pending.prototype.run = function run(job) {
+  return job();
 };
 
 States.Online = function Online(taskRunner) {
@@ -69,43 +62,37 @@ States.Online.prototype.runJobs = async function runJobs() {
     return this.taskRunner.setState(this.taskRunner.states.idle.getState());
   }
 
-  if (this.taskRunner.state !== "online") {
-    return this.taskRunner.setState(this.taskRunner.states.pending.getState());
+  if (!this.taskRunner.inState("online")) {
+    return this.taskRunner.setState("pending");
   }
 
   for (const job of this.taskRunner.jobQueue) {
-    if (this.taskRunner.state !== "online") {
+    if (!this.taskRunner.inState("online")) {
       break;
     }
-    await job.exec().then(() => (job.done = true));
+    await job.exec().then(() => {
+      job.done = true;
+    });
   }
 
   this.runJobs();
 };
 
-States.Online.prototype.run = function run(task, timeout) {
-  return new Promise((resolve, reject) => {
-    const job = {
-      timeout,
-      done: false,
-      exec: () => task().then(resolve, reject),
-    };
-    this.taskRunner.queue(job);
-    this.runJobs();
-  });
+States.Online.prototype.run = function run(job) {
+  return job();
 };
 
 function TaskRunner(config = {}) {
   const conf = this.parseConfig(config);
   this.timeout = conf.timeout;
   this.jobQueue = [];
-  this.state = "";
+  this.state = null;
   this.states = {
     idle: new States.Idle(this),
     pending: new States.Pending(this),
     online: new States.Online(this),
   };
-  this.setState(this.states.idle.getState());
+  this.setState("idle");
 }
 
 TaskRunner.prototype.parseConfig = function parseConfig(config) {
@@ -116,31 +103,57 @@ TaskRunner.prototype.parseConfig = function parseConfig(config) {
 
 TaskRunner.prototype.setState = function (state) {
   const oldState = `[TRANSITION]:taskRunner ${this.state?.name}`;
-  this.state = state;
+  this.state = this.states[state];
   console.log(`${oldState} -> ${this.state.name}`);
-  if (Object.hasOwn(state.init)) {
-    state.init();
+  if (!this.state) {
+    throw new TaskRunnerError(`Unrecognized state ${state}`);
+  }
+
+  if ("init" in this.state) {
+    this.state.init();
   }
 };
 
 TaskRunner.prototype.queue = function (job) {
-  this.jobQueue.push(job);
+  return this.jobQueue.push(job);
 };
 
 TaskRunner.prototype.flush = function () {
   const now = Date.now();
   this.jobQueue = this.jobQueue.filter((job) => {
     if (now > job.timeout) {
-      job.done = true;
-      job.execute();
+      job.exec(true); // job expired
     }
-    return !job.done;
+    return now < job.timeout;
   });
 };
 
+TaskRunner.prototype.newJob = function (task, options) {
+  return (cb) =>
+    new Promise((resolve, reject) => {
+      this.queue({
+        timeout: Date.now() + (options.timeout || this.timeout),
+        exec: (expired) => {
+          if (expired) {
+            return reject(new TaskRunnerError("Task timeout"));
+          }
+          try {
+            task().then(resolve, reject);
+          } catch (err) {
+            reject(new TaskRunnerError("Task not a promise"));
+          }
+        },
+      });
+      cb();
+    });
+};
+
+TaskRunner.prototype.inState = function (state) {
+  return this.state.name === state;
+};
+
 TaskRunner.prototype.run = function (task, options = {}) {
-  const timeout = Date.now() + (options.timeout || this.timeout);
-  return this.state.run(task, timeout);
+  return this.state.run(this.newJob(task, options));
 };
 
 export { TaskRunner };
--- tests/taskRunner.test.js
+++ tests/taskRunner.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { TaskRunner } from "/src/task_runners/index.js";
 
 describe("task runner", () => {
@@ -24,5 +24,28 @@ describe("task runner", () => {
       timeout: 6000,
     });
     expect(tr).toMatchObject(expectedCustomConfig);
+  });
+  it("Should transition from idle to pending when tasks are provided and service is offline", () => {
+    const tr = new TaskRunner();
+    tr.run(() => console.log("task #1"));
+    expect(tr.jobQueue.length).toEqual(1);
+    expect(tr.inState("pending")).toBe(true);
+  });
+
+  it("Should transition from pending to idle when all tasks have been flushed due to timeout", () => {
+    vi.useFakeTimers();
+    const tr = new TaskRunner();
+    const task = () =>
+      new Promise((resolve, reject) => {
+        resolve("task 1");
+      });
+
+    tr.run(task).catch((err) => console.log(err));
+    vi.advanceTimersToNextTimer();
+    vi.advanceTimersToNextTimer();
+    vi.advanceTimersToNextTimer();
+    vi.advanceTimersToNextTimer();
+    expect(tr.inState("idle")).toBe(true);
+    vi.useRealTimers();
   });
 });
